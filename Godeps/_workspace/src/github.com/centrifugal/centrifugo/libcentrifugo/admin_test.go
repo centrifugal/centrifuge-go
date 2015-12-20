0c9fb3626312e57e92f09f89f1d57e704aea3ca1
FZambia <frvzmb@gmail.com>
2015-12-20 11:14:29 +0300

    revert glide related commits for a while

---
+++ Godeps/_workspace/src/github.com/centrifugal/centrifugo/libcentrifugo/admin_test.go
@@ -0,0 +1,65 @@
+package libcentrifugo
+
+import (
+	"testing"
+
+	"github.com/gorilla/securecookie"
+	"github.com/stretchr/testify/assert"
+)
+
+type testAdminSession struct{}
+
+func (s *testAdminSession) WriteMessage(int, []byte) error {
+	return nil
+}
+
+func newAdminTestConfig() *Config {
+	return &Config{
+		WebSecret: "secret",
+	}
+}
+
+func newAdminTestApplication() *Application {
+	app, _ := NewApplication(newAdminTestConfig())
+	return app
+}
+
+func newTestAdminClient() (*adminClient, error) {
+	app := newAdminTestApplication()
+	c, err := newAdminClient(app, &testAdminSession{})
+	return c, err
+}
+
+func TestAdminClient(t *testing.T) {
+	c, err := newTestAdminClient()
+	go c.writer()
+	assert.Equal(t, nil, err)
+	assert.NotEqual(t, c.uid(), "")
+	err = c.send([]byte("message"))
+	assert.Equal(t, nil, err)
+}
+
+func TestAdminClientMessageHandling(t *testing.T) {
+	c, err := newTestAdminClient()
+	assert.Equal(t, nil, err)
+	emptyMsg := ""
+	_, err = c.handleMessage([]byte(emptyMsg))
+	assert.NotEqual(t, nil, err)
+	malformedMsg := "ooops"
+	_, err = c.handleMessage([]byte(malformedMsg))
+	assert.NotEqual(t, nil, err)
+	unknownMsg := "{\"method\":\"unknown\", \"params\": {}}"
+	_, err = c.handleMessage([]byte(unknownMsg))
+	assert.Equal(t, ErrInvalidMessage, err)
+	emptyAuthMethod := "{\"method\":\"auth\", \"params\": {}}"
+	_, err = c.handleMessage([]byte(emptyAuthMethod))
+	assert.Equal(t, ErrUnauthorized, err)
+	s := securecookie.New([]byte(c.app.config.WebSecret), nil)
+	token, _ := s.Encode(AuthTokenKey, AuthTokenValue)
+	correctAuthMethod := "{\"method\":\"auth\", \"params\": {\"token\":\"" + token + "\"}}"
+	_, err = c.handleMessage([]byte(correctAuthMethod))
+	assert.Equal(t, nil, err)
+	pingCommand := "{\"method\":\"ping\", \"params\": {}}"
+	_, err = c.handleMessage([]byte(pingCommand))
+	assert.Equal(t, nil, err)
+}
---
+++ Godeps/_workspace/src/github.com/centrifugal/centrifugo/libcentrifugo/errors.go
@@ -0,0 +1,20 @@
+package libcentrifugo
+
+import (
+	"errors"
+)
+
+var (
+	ErrInvalidMessage      = errors.New("invalid message")
+	ErrInvalidToken        = errors.New("invalid token")
+	ErrUnauthorized        = errors.New("unauthorized")
+	ErrMethodNotFound      = errors.New("method not found")
+	ErrPermissionDenied    = errors.New("permission denied")
+	ErrNamespaceNotFound   = errors.New("namespace not found")
+	ErrInternalServerError = errors.New("internal server error")
+	ErrLimitExceeded       = errors.New("limit exceeded")
+	ErrNotAvailable        = errors.New("not available")
+	ErrConnectionExpired   = errors.New("connection expired")
+	ErrSendTimeout         = errors.New("send timeout")
+	ErrClientClosed        = errors.New("client is closed")
+)
