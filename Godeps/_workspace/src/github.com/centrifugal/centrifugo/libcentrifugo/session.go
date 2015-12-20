58c8e24db330716af49e29dafad1f8a20f0a0b2b
FZambia <frvzmb@gmail.com>
2015-12-20 10:48:11 +0300

    glide for dependency management

--- Godeps/_workspace/src/github.com/centrifugal/centrifugo/libcentrifugo/session.go
+++
@@ -1,9 +0,0 @@
-package libcentrifugo
-
-// Session represents a connection between server and client.
-type session interface {
-	// Send sends one message to session
-	Send([]byte) error
-	// Close closes the session with provided code and reason.
-	Close(status uint32, reason string) error
-}
