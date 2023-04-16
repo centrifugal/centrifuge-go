29022a8f939642e47765c4a7a6e146a1d28362ba
Alexander Emelin <frvzmb@gmail.com>
2023-04-16 18:41:49 +0300

    Up protocol, rm unused fields (#77)

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/centrifugal/centrifuge-go
 go 1.16
 
 require (
-	github.com/centrifugal/protocol v0.8.11
+	github.com/centrifugal/protocol v0.10.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/jpillora/backoff v1.0.0
 	github.com/segmentio/asm v1.2.0 // indirect
--- protocol.go
+++ protocol.go
@@ -48,39 +48,11 @@ func (e Error) Error() string {
 	return fmt.Sprintf("%d: %s", e.Code, e.Message)
 }
 
-func newPushEncoder(enc protocol.Type) protocol.PushEncoder {
-	if enc == protocol.TypeJSON {
-		return protocol.NewJSONPushEncoder()
-	}
-	return protocol.NewProtobufPushEncoder()
-}
-
-func newPushDecoder(enc protocol.Type) protocol.PushDecoder {
-	if enc == protocol.TypeJSON {
-		return protocol.NewJSONPushDecoder()
-	}
-	return protocol.NewProtobufPushDecoder()
-}
-
 func newReplyDecoder(enc protocol.Type, data []byte) protocol.ReplyDecoder {
 	if enc == protocol.TypeJSON {
 		return protocol.NewJSONReplyDecoder(data)
 	}
 	return protocol.NewProtobufReplyDecoder(data)
-}
-
-func newResultDecoder(enc protocol.Type) protocol.ResultDecoder {
-	if enc == protocol.TypeJSON {
-		return protocol.NewJSONResultDecoder()
-	}
-	return protocol.NewProtobufResultDecoder()
-}
-
-func newParamsEncoder(enc protocol.Type) protocol.ParamsEncoder {
-	if enc == protocol.TypeJSON {
-		return protocol.NewJSONParamsEncoder()
-	}
-	return protocol.NewProtobufParamsEncoder()
 }
 
 func newCommandEncoder(enc protocol.Type) protocol.CommandEncoder {
